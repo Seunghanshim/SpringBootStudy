53d822d88fb3319a570baebe548ae73b81379af7
Seunghanshim <seunghan_dev@naver.com>
2020-08-18 22:09:23 +0900

    feat: Update Item domain(Add business Logic)

--- src/main/java/Study/Spring/boot/study/domain/Item/Item.java
+++ src/main/java/Study/Spring/boot/study/domain/Item/Item.java
@@ -1,6 +1,7 @@
 package Study.Spring.boot.study.domain.Item;
 
 import Study.Spring.boot.study.domain.Category;
+import Study.Spring.boot.study.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -23,4 +24,17 @@ public class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    //비즈니스 로직
+    public void addStock(int quantity){
+        this.stockQuantity += quantity;
+    }
+
+    public void removeStock(int quantity){
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0){
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
