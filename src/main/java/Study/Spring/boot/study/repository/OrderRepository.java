2ce74dbd3eca9f621c4f9efa69bcb093b10edde4
Seunghanshim <seunghan_dev@naver.com>
2020-08-19 16:52:13 +0900

    feat: Add repository(Order)

---
+++ src/main/java/Study/Spring/boot/study/repository/OrderRepository.java
@@ -0,0 +1,21 @@
+package Study.Spring.boot.study.repository;
+
+import Study.Spring.boot.study.domain.Order;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepository {
+    private final EntityManager em;
+
+    public void save(Order order){
+        em.persist(order);
+    }
+
+    public Order findOne(Long id){
+        return em.find(Order.class, id);
+    }
+}
