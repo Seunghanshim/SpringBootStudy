751f12911070ab5ac521258372a2c44aa20115cd
Seunghanshim <seunghan_dev@naver.com>
2020-08-19 16:49:46 +0900

    feat: Add create method & business logic(Order, OrderItem)

--- src/main/java/Study/Spring/boot/study/domain/Order.java
+++ src/main/java/Study/Spring/boot/study/domain/Order.java
@@ -47,4 +47,39 @@ public class Order {
         this.delivery = delivery;
         delivery.setOrder(this);
     }
+
+    //생성 메서드
+    public Order CreateOrder(Member member, Delivery delivery, OrderItem... orderItems){
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+        for(OrderItem orderItem: orderItems){
+            order.getOrderItems().add(orderItem);
+        }
+        order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+
+        return order;
+    }
+
+    //비즈니스 로직
+    public void cancel(){
+        if(this.delivery.getStatus() == DeliveryStatus.COMP){
+            throw new IllegalStateException("이미 배송완료된 상품으 취소가 불가능합니다.");
+        }
+
+        this.setStatus(OrderStatus.CANCEL);
+        for(OrderItem orderItem: this.orderItems){
+            orderItem.cancel();
+        }
+    }
+
+    //조회 로직
+    public int getTotalPrice(){
+        int totalPrice = 0;
+        for(OrderItem orderItem: this.orderItems){
+            totalPrice += orderItem.getTotalPrice();
+        }
+        return totalPrice;
+    }
 }
